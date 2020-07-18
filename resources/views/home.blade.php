603afc152fb63aa440bf61ef8638cb9b8c0b1d5d
Meutya Suryadana <43804907+mutyazen@users.noreply.github.com>
2020-07-18 07:20:26 +0700

    no message

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,18 +3,49 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+        <div class="col-md-12">
+            <div class="row">
+                <div class="col-lg-4 col-md-12 py-4">
+                    <div class="card">
+                        <div class="card-header">Library 1</div>
+                        <div class="card-body">
+                            <div class="col">
+                                <div class="row">
+                                    <div class="col-4">Book A</div>
+                                    <div class="col-4">Book B</div>
+                                    <div class="col-4">Book C</div>
+                                </div>
+                            </div>
                         </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-12 py-4">
+                    <div class="card">
+                        <div class="card-header">Library 1</div>
+                        <div class="card-body">
+                            <div class="col-md-12">
+                                <div class="row">
+                                    <div class="col-4">Book A</div>
+                                    <div class="col-4">Book B</div>
+                                    <div class="col-4">Book C</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-12 py-4">
+                    <div class="card">
+                        <div class="card-header">Library 1</div>
+                        <div class="card-body">
+                            <div class="col-md-12">
+                                <div class="row">
+                                    <div class="col-4">Book A</div>
+                                    <div class="col-4">Book B</div>
+                                    <div class="col-4">Book C</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -59,6 +59,9 @@
                                 </li>
                             @endif
                         @else
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">List Library</a>
+                            </li>
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }} <span class="caret"></span>
